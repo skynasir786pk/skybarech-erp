04347076d9ed8be23d50295f85adce2de6f8062e
SkyBarech ERP Build <build@skybarech.local>
2026-09-21 01:18:00 +0500

    Import localized thermal printer text

--- 02-Android-Client/app/src/main/java/com/skybarech/mobileshoperp/ui/components/ThermalPrinter.kt
+++ 02-Android-Client/app/src/main/java/com/skybarech/mobileshoperp/ui/components/ThermalPrinter.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.core.content.ContextCompat
+import com.skybarech.mobileshoperp.ui.i18n.UiText
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
